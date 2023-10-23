1ed45018a2a9ef864c18caf918217d4c7325897c
jdeyerle <jdeyerle.dev@pm.me>
2023-10-22 18:52:09 -0700

    feat: markdown preview

--- lua/jdeyerle/plugins/extras.lua
+++ lua/jdeyerle/plugins/extras.lua
@@ -10,4 +10,14 @@ return {
       vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndotree' })
     end,
   },
+
+  -- trying prebuild to see if it prevents update conflicts
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
