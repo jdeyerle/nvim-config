da9128959495fbf0b132832dffb89f12f215529b
jdeyerle <jdeyerle.dev@pm.me>
2024-06-16 20:32:10 -0700

    feat: inc-rename

--- lua/jdeyerle/plugins/text-editing.lua
+++ lua/jdeyerle/plugins/text-editing.lua
@@ -1,9 +1,16 @@
 return {
   {
+    'smjonas/inc-rename.nvim',
+    opts = {},
+    keys = { { '<leader>rn', ':IncRename ', '[R]e[n]ame' } },
+  },
+
+  {
     'tpope/vim-commentary',
     config = function()
       vim.cmd 'autocmd FileType gleam setlocal commentstring=//%s'
     end,
   },
+
   { 'tpope/vim-surround' },
 }
